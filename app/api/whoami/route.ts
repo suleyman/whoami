bd9c72c77e14cfe2d8f930714460c74c1d1b7882
suleyman <slymnd28@gmail.com>
2023-07-23 18:38:16 +0300

    update env

--- app/api/whoami/route.ts
+++ app/api/whoami/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   const { ip, geo } = request;
   return NextResponse.json({
-    ip: "127.0.0.1",
-    location: "Istanbul, Turkey",
+    ip: ip || "127.0.0.1",
+    location: geo?.city || "Istanbul, Turkey",
   });
 }
